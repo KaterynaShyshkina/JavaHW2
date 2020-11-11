5f4200412b3cf2433cc4e74de232e2c9222731cf
Kateryna Shyshkina <ekateryna.shyshkina@gmail.com>
2020-11-11 18:54:29 +0200

    unit test added

--- src/test/java/org/game/MethodNameTests.java
+++ src/test/java/org/game/MethodNameTests.java
@@ -1,13 +1,19 @@
 package org.game;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 public class MethodNameTests {
+
     @Test
-    public void checkMethodName() {
+    public void checkThatMethodIsExist() {
+
+        String methodName = "checkValue";
         try {
-            var method = Model.class.getMethod("checkValue").getName();
+            var method = Model.class.getMethod(methodName).getName();
         } catch (NoSuchMethodException e) {
+            fail(methodName + " method is not found");
             e.printStackTrace();
         }
     }
